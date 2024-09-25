8c6fdefcc51442b4e6061c5a265b8cf75e947b9f
Shashank <shashankprabhakar@icloud.com>
2024-09-25 14:21:12 +0530

    intial commnit CardView

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -7,18 +7,61 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
 
-#Preview {
-    ContentView()
+
+    
+    struct ContentView: View {
+        var body: some View {
+            
+            
+            HStack {
+                
+                CardView()
+                
+                CardView()
+                    CardView()
+                    CardView()
+                    CardView()
+                }
+                .foregroundColor(.orange)
+                .padding()
+            }
+            
+        }
+    
+
+struct CardView: View {
+    
+    var isFaceUp: Bool = true
+        
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12).foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 26).stroke(lineWidth: 2)
+                
+                Text("🐻").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+            }
+            
+        }
+    }
+
 }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    #Preview {
+        ContentView()
+    }
+
+        
